81fa6046884cf87c1b8ea181560505a02bb1faf5
totally google drive <>
2023-02-12 04:12:47 +0000

    Initial commit

---
+++ hh scripts/hh.js
@@ -0,0 +1,177 @@
+        function openGame() {
+      var win = window.open()
+      win.document.title = "Enter Password";
+      var url = "https://totallynewtabforthethirdtime-666-111-777-111-100-555-003.totallygoogledrive.repl.co/passwordthingy"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+    function eagleCraft() {
+      var win = window.open()
+      win.document.title = "Eaglecraft";
+      var url = "https://eaglercraft-server.totallygoogledrive.repl.co"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+    }
+
+    function google() {
+      var win = window.open()
+      win.document.title = "Bing";
+      var url = "https://bing.com"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "black";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+    }
+
+        function public() {
+      var win = window.open()
+      win.document.title = "Chatroom";
+      var url = "https://Anothernewtabbutcool.totallygoogledrive.repl.co/publicroom"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function spookySchoolTest() {
+      var win = window.open()
+          win.document.title = "Spooky Ooky School";
+      var url = "https://playcanv.as/b/817620b0"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+        function yahzee() {
+      var win = window.open()
+      var url = "https://www.youtube.com"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+    function link() {
+      var win = window.open()
+      win.document.title = "Link Inserter";
+      var url = "https://totallynewtab.totallygoogledrive.repl.co"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+    function itch() {
+      var win = window.open()
+      win.document.title = "Flight Radar";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/embedded/radar.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+    function rotate() {
+      var win = window.open()
+      win.document.title = "Rotate";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/embedded/rotate.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function rumbleverse() {
+      var win = window.open()
+      win.document.title = "Rumbleverse Videos";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/other/rumbleversevideo.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function justFall() {
+      var win = window.open()
+      win.document.title = "Just Fall";
+      var url = "https://justfall.lol"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function rumbleMusic() {
+      var win = window.open()
+      win.document.title = "Music Hub";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/other/rumbleversemusic.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function changeLog() {
+      var win = window.open()
+      win.document.title = "Changelog";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/other/changelog.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function gpt() {
+      var win = window.open()
+      win.document.title = "ChatGPT";
+      var url = "https://totallyaboutblank-nothing-else.totallygoogledrive.repl.co/other/gpt.html"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
+
+        function youtube() {
+      var win = window.open()
+      win.document.title = "Youtube";
+      var url = "https://tube.cadence.moe/"
+      var iframe = win.document.createElement('iframe')
+      iframe.style.width = "100%";
+      iframe.style.height = "100%";
+      iframe.style.border = "none";
+      iframe.src = url
+      win.document.body.appendChild(iframe)
+        }
