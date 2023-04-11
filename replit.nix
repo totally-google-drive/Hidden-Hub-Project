6b9f6ada66291bef87bbdf1a9eb9d5d946ddd84e
t-g-d <125172092+totally-google-drive@users.noreply.github.com>
2023-04-11 18:09:16 -0500

    Delete replit.nix

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs-16_x
-    pkgs.python39Packages.pip
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
-  ];
-}
