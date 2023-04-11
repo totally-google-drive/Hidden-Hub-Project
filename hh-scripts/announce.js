0affb121cf5f092418c3e5dc5448ad0de801010b
t-g-d <125172092+totally-google-drive@users.noreply.github.com>
2023-04-11 18:10:38 -0500

    Delete hh-scripts directory

--- hh-scripts/announce.js
+++
@@ -1,6 +0,0 @@
-const showDivButton = document.querySelector("#show-div");
-const contentDiv = document.querySelector("#content");
-
-showDivButton.addEventListener("click", function() {
-  contentDiv.classList.toggle("hidden");
-});
